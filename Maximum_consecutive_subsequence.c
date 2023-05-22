f9f2322ee644d61961d4348a813e09b4e0947437
Leonardo Braun <leonardolbraun@gmail.com>
2023-05-22 14:58:18 -0400

    Refactor: programa para encontrar a sequencia consecutiva maxima

--- Maximum_consecutive_subsequence.c
+++ Maximum_consecutive_subsequence.c
@@ -1,125 +1,123 @@
 #include <stdio.h>
 /*
-subMaxArray() é uma função iterativa que retorna o valor da soma dos
-elementos da  subsequência máxima de um outro arranjo chamado x.
+Maber pág. 106 - 5.8 - Finding the Maximum Consecutive Subsequence
+
+encontrarSubsequenciaMaxima (subMaxArray()) é uma função iterativa que retorna o valor da soma dos
+elementos da  subsequência máxima de um outro arranjo chamado arr (x).
 Além disso, retorna por referência os arranjos com a subsequência
 máxima e o sufixo máximo.
 
 Parâmetros:
-x : o ponteiro para o arranjo para o qual será determinada a subsequência máxima;
-n : tamanho de x;
-sfmarray : ponteiro para o arranjo com os elementos que formam o sufixo máximo;
-nsfm : ponteiro para a variável que contém o tamanho de sfmarray;
-gbmarray : ponteiro para o arranjo com os elementos que formam a subsequência máxima;
-ngbm : ponteiro para a variável que contém o tamanho de gbmarray.
+arr (x) : o ponteiro para o arranjo para o qual será determinada a subsequência máxima;
+size (n) : tamanho de x;
+sufixoMaxArray (sfmarray) : ponteiro para o arranjo com os elementos que formam o sufixo máximo;
+tamanhoSufixoMax (nsfm) : ponteiro para a variável que contém o tamanho de sufixoMaxArray (sfmarray);
+subsequenciaMaximaArray (gbmarray) : ponteiro para o arranjo com os elementos que formam a subsequência máxima;
+tamanhoSubsequenciaMaxima (ngbm) : ponteiro para a variável que contém o tamanho de subsequenciaMaximaArray (gbmarray).
 
 Valor de retorno: soma da subsequência máxima.
 
 */
-double subMaxArray(double *x, int n, double * sfmarray, int * nsfm,
-  double * gbmarray, int * ngbm) {
-  double globalmax = 0, sufixmax = 0;
-  *nsfm = 0; *ngbm = 0;
+double encontrarSubsequenciaMaxima(double *arr, int size, double *sufixoMaxArray, int *tamanhoSufixoMax,
+                                  double *subsequenciaMaximaArray, int *tamanhoSubsequenciaMaxima) {
+    double globalMaximo = 0, sufixoMaximo = 0;
+    *tamanhoSufixoMax = 0;
+    *tamanhoSubsequenciaMaxima = 0;
 
-  for(int i = 0; i < n; i++) {
-    printf("sufixmax: %.2f, globalmax: %.2f, x[%d]: %.2f\n", sufixmax, globalmax, i, x[i]);
-    printf("Digite um caracter para continuar...");
-    getchar();
-    if(x[i]+sufixmax > globalmax)
-    {
-      sufixmax = x[i]+sufixmax;
-      sfmarray[*nsfm] = x[i];
-      (*nsfm)++;
-      globalmax = sufixmax;
-      for(int j = 0; j < *nsfm; ++j)
-      {
-        gbmarray[j] = sfmarray[j];
-      }
-      *ngbm = *nsfm;
-      printf("Novo global máximo == sulfixo máximo: [");
-            for(int i = 0; i < *nsfm; ++i)
-              printf("%.2f ", sfmarray[i]);
-            printf("]\n");
-    }
-    else {
-      if (x[i]+sufixmax > 0)
-          {
-            sufixmax = x[i]+sufixmax;
-            sfmarray[*nsfm] = x[i];
-            (*nsfm)++;
-            printf("Novo Sulfixo máximo: [");
-            for(int i = 0; i < *nsfm; ++i)
-              printf("%.2f ", sfmarray[i]);
-            printf("]\n");
-            printf("Global máximo: [");
-            for(int i = 0; i < *ngbm; ++i)
-              printf("%.2f ", gbmarray[i]);
-            printf("]\n");
-          }
-      else
-        {
-          sufixmax = 0;
-          *nsfm=0;
-          printf("Novo globa máximo == sulfixo máximo: []\n");
+    for (int i = 0; i < size; i++) {
+        printf("Sufixo máximo: %.2f, Global máximo: %.2f, arr[%d]: %.2f\n", sufixoMaximo, globalMaximo, i, arr[i]);
+        printf("Digite um caractere para continuar...");
+        getchar();
 
+        if (arr[i] + sufixoMaximo > globalMaximo) {
+            sufixoMaximo = arr[i] + sufixoMaximo;
+            sufixoMaxArray[*tamanhoSufixoMax] = arr[i];
+            (*tamanhoSufixoMax)++;
+            globalMaximo = sufixoMaximo;
+
+            for (int j = 0; j < *tamanhoSufixoMax; ++j) {
+                subsequenciaMaximaArray[j] = sufixoMaxArray[j];
+            }
+            *tamanhoSubsequenciaMaxima = *tamanhoSufixoMax;
+
+            printf("Novo global máximo == sufixo máximo: [");
+            for (int i = 0; i < *tamanhoSufixoMax; ++i) {
+                printf("%.2f ", sufixoMaxArray[i]);
+            }
+            printf("]\n");
+        } else {
+            if (arr[i] + sufixoMaximo > 0) {
+                sufixoMaximo = arr[i] + sufixoMaximo;
+                sufixoMaxArray[*tamanhoSufixoMax] = arr[i];
+                (*tamanhoSufixoMax)++;
+
+                printf("Novo sufixo máximo: [");
+                for (int i = 0; i < *tamanhoSufixoMax; ++i) {
+                    printf("%.2f ", sufixoMaxArray[i]);
+                }
+                printf("]\n");
+
+                printf("Global máximo: [");
+                for (int i = 0; i < *tamanhoSubsequenciaMaxima; ++i) {
+                    printf("%.2f ", subsequenciaMaximaArray[i]);
+                }
+                printf("]\n");
+            } else {
+                sufixoMaximo = 0;
+                *tamanhoSufixoMax = 0;
+                printf("Novo global máximo == sufixo máximo: []\n");
+            }
         }
     }
-  }
-  return globalmax;
+    return globalMaximo;
 }
 
-double subMax(double *x, int n) {
-  double globalmax = 0, sufixmax = 0;
+double encontrarSubsequenciaMaximaSemArray(double *arr, int size) {
+    double globalMaximo = 0, sufixoMaximo = 0;
 
-  for(int i =0; i < n; i++) {
-    if(x[i] +sufixmax > globalmax)
-    {
-      sufixmax = x[i] +sufixmax;
-      globalmax = sufixmax;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] + sufixoMaximo > globalMaximo) {
+            sufixoMaximo = arr[i] + sufixoMaximo;
+            globalMaximo = sufixoMaximo;
+        } else {
+            if (arr[i] + sufixoMaximo > 0) {
+                sufixoMaximo = arr[i] + sufixoMaximo;
+            } else {
+                sufixoMaximo = 0;
+            }
+        }
     }
-    else {
-      if (x[i] +sufixmax > 0)
-          sufixmax = x[i] +sufixmax;
-      else
-        sufixmax = 0;
-    }
-  }
-  return globalmax;
+    return globalMaximo;
 }
-
 
 int main() {
-    int i, noErros = 0, noTestes = 0;
-    double a[] ={2,-3, 1.5, -1, 3, -2, -3, -3, 8};
-    double b[] ={2,-3, 5, -1, 3, -2, 13, 3};
-    double c[] ={-2,-3, 5, -1, -3, 2, -3, -3};
+    double arrayA[] = {2, -3, 1.5, -1, 3, -2, -3, -3, 8};
+    double arrayB[] = {2, -3, 5, -1, 3, -2, 13, 3};
+    double arrayC[] = {-2, -3, 5, -1, -3, 2, -3, -3};
 
-    double globalmax, sufixmax;
-    double globalmax_array[9], sufixmax_array[9];
-    int n_globalmax_array, n_sufixmax_array;
+    double globalMaximo, sufixoMaximo;
+    double subsequenciaMaximaArray[9], sufixoMaxArray[9];
+    int tamanhoSubsequenciaMaxima, tamanhoSufixoMax;
 
     printf("Vetor: [");
-    for(i = 0; i < 9; ++i)
-      printf("%3.2f ", a[i]);
+    for (int i = 0; i < 9; ++i) {
+        printf("%3.2f ", arrayA[i]);
+    }
     printf("]\n");
 
-    printf("Globalmax-Sem-Array: %3.2f\n", subMax(a,9));
-    // globalmax = rsubMax(a,8,&sufixmax);
-    // printf("===> Calculando os valores da subsequência máxima e do sufixo máximo\n");
-    // printf("Globalmax: %3.2f\n", globalmax);
-    // printf("Sufixmax: %3.2f\n", sufixmax);
+    printf("Global máximo (Sem Array): %3.2f\n", encontrarSubsequenciaMaximaSemArray(arrayA, 9));
 
+    globalMaximo = encontrarSubsequenciaMaxima(arrayA, 9, sufixoMaxArray, &tamanhoSufixoMax,
+                                               subsequenciaMaximaArray, &tamanhoSubsequenciaMaxima);
 
-    globalmax = subMaxArray(a,9,sufixmax_array,&n_sufixmax_array, globalmax_array,&n_globalmax_array);
-
-    printf("subMaxArray:\n");
-    printf("Globalmax: %3.2f\n", globalmax);
-    printf("Sufixmax: %3.2f\n", sufixmax);
+    printf("Subsequência Máxima:\n");
+    printf("Global máximo: %3.2f\n", globalMaximo);
+    printf("Sufixo máximo: %3.2f\n", sufixoMaximo);
     printf("Subsequência máxima correta: [ ");
-    for(i = 0; i < n_globalmax_array; ++i)
-      printf("%3.2f ", globalmax_array[i]);
+    for (int i = 0; i < tamanhoSubsequenciaMaxima; ++i) {
+        printf("%3.2f ", subsequenciaMaximaArray[i]);
+    }
     printf("]\n");
 
-    
-  return 0;
+    return 0;
 }
